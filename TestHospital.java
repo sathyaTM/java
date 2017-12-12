cded6cd6ac6965f6261dacbea11d9e7b7a46ecf1
Sathyanarayanan Sundaravadivelu <sathya82in@gmail.com>
2017-12-12 23:02:27 +0000

    TestHospital.java

---
+++ TestHospital.java
@@ -0,0 +1,17 @@
+package hospitalProj;
+
+public class TestHospital {
+
+	public static void main(String[] args)
+	{
+		Hospital f=new FortisHospital();
+		f.doScan();
+		f.doOperation();
+		f.doVaccination();
+
+		
+		
+	
+	}
+
+}
